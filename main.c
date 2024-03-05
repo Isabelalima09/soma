99459806eda2656012d6dbe84d92be4774fd89b6
Isabelalima09 <162131034+Isabelalima09@users.noreply.github.com>
2024-03-05 12:22:42 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int soma ( int num1, int num2){
+  int soma = num1 + num2;
+printf( " a soma dos numero é: %d \n", soma);
+  return soma;
+}
+int main(void) {
+ soma ( 10 , 20);
+  
+}
